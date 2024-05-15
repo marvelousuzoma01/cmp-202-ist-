b11386e1d4ecb35b9f094b611d75ff2fccfb1382
marvelousuzoma01 <130617409+marvelousuzoma01@users.noreply.github.com>
2024-05-15 23:24:35 +0100

    Create user.java

---
+++ user.java
@@ -0,0 +1,25 @@
+public class User {
+    String username = "marvelous";
+    String password = "Pass";
+    String login() {
+        if (username.equals("marvelous") && password.equals("Pass")) {
+            return "Logged in";
+        } else {
+            return "Failed";
+        }
+    }
+    boolean checker() {
+        if (3 != 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public static void main(String[] args) {
+        User jane = new User();
+        User luke = new User();
+        System.out.println(luke.checker());
+        System.out.println(jane.login());
+    }
+}
