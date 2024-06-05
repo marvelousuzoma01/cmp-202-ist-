57bc559f20ce5c9fc5498de7e0fc1ea212605e26
marvelousuzoma01 <130617409+marvelousuzoma01@users.noreply.github.com>
2024-06-05 07:28:56 +0100

    Create persons.java

---
+++ persons.java
@@ -0,0 +1,11 @@
+
+package OOP_java.Users;
+
+public class Person extends Users {
+    String lastName;
+
+    public static void main(String[] args) {
+        Person betty = new Person();
+        System.out.println(betty.getName());
+    }    
+}
